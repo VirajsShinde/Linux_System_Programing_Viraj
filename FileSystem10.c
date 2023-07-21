b59700347bf6076468fb33d3669706f46d2a3476
Viraj Sunil Shinde <119616651+VirajsShinde@users.noreply.github.com>
2023-07-21 14:43:34 -0400

    Add files via upload

---
+++ FileSystem10.c
@@ -0,0 +1,32 @@
+//=================================================
+// Linux System Programing | Viraj_Shinde         
+//=================================================
+
+#include<stdio.h>
+#include<fcntl.h>
+
+int main(int argc, char *argv[])
+{
+    int fd = 0;
+
+    printf("Usage : Name_Of_Executable Name_Of_File\n");
+    
+    if(argc != 2)
+    {
+        printf("Invalid number of arguments\n");
+        return -1;
+    }
+
+    fd = creat(argv[1], 0777);   //0777-R,W,E Permission
+    if(fd == -1)
+    {
+        printf("Unable to create file\n");
+        return -1;
+    }
+    else
+    {
+        printf("File is succesfully created with fd : %d\n",fd);
+    }
+
+    return 0;
+}
